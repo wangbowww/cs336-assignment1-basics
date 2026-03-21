494912742a2495c773a6150ff653319f5b1a1531
wangbowww <weirdowww@qq.com>
2026-03-21 07:03:43 +0000

    passed train_bpe, still have space to progress

--- cs336_basics/BPE_tokenizer.py
+++ cs336_basics/BPE_tokenizer.py
@@ -46,67 +46,62 @@ def merge(
 def _init_buffer(
     pre_tokens: dict[tuple[bytes, ...], int],
     pair_freq_buffer: dict[tuple[bytes, bytes], int],
-    pair_freq_index_buffer: dict[tuple[bytes, bytes], list[tuple[bytes, ...]]]
+    pair_pretoken_buffer: dict[tuple[bytes, bytes], set[tuple[bytes, ...]]],
 ):
     for pre_token, freq in pre_tokens.items():
         for i in range(len(pre_token) - 1):
             pair = (pre_token[i], pre_token[i+1])
             pair_freq_buffer[pair] = pair_freq_buffer.get(pair, 0) + freq
-            pair_freq_index_buffer.setdefault(pair, []).append(pre_token)
+            pair_pretoken_buffer.setdefault(pair, set()).add(pre_token)
 
-def _update_pre_tokens_with_buffer(
+def _update_with_buffer(
     pre_tokens: dict[tuple[bytes, ...], int],
     pair_freq_buffer: dict[tuple[bytes, bytes], int],
-    pair_freq_index_buffer: dict[tuple[bytes, bytes], list[tuple[bytes, ...]]]
+    pair_pretoken_buffer: dict[tuple[bytes, bytes], set[tuple[bytes, ...]]]
 ):
-    # some pre_tokens has been removed, but pair_freq_index_buffer[pair] = list[...pre_token...]
-    # so it will occur "Key Error"
     assert len(pair_freq_buffer) > 0
     # get best pair from buffer
     best_pair, best_freq = max(pair_freq_buffer.items(), key=lambda x: (x[1], x[0]))
-    # update pre_tokens with buffers
-    new_pairs: dict[tuple[bytes, bytes], int] = {}
-    old_pairs: dict[tuple[bytes, bytes], int] = {}
-    new_pairs_index: dict[tuple[bytes, bytes], list[tuple[bytes, ...]]] = {}
-    for pre_token in pair_freq_index_buffer[best_pair]:
+    # first update pair_pretoken_buffer for previous runs
+    del_pre_tokens = []
+    for pre_token in pair_pretoken_buffer[best_pair]:
+        if pre_token not in pre_tokens:
+            del_pre_tokens.append(pre_token)
+    for del_pre_token in del_pre_tokens:
+        pair_pretoken_buffer[best_pair].remove(del_pre_token)
+    # update pre_tokens and buffers
+    old_pair_freq = {}
+    new_pair_freq = {}
+    for pre_token in pair_pretoken_buffer[best_pair]:
+        freq = pre_tokens[pre_token]
         new_pre_token: list[bytes] = []
         i = 0
-        freq = pre_tokens[pre_token]
-        tmp_new_pairs: dict[tuple[bytes, bytes], int] = {}
         while i < len(pre_token):
             if i+1 < len(pre_token) and (pre_token[i], pre_token[i+1]) == best_pair:
-                # we should merge pre_token[i] and pre_token[i+1]
                 new_token = pre_token[i] + pre_token[i+1]
                 new_pre_token.append(new_token)
-                if i > 0:
-                    old_pair = (pre_token[i-1], pre_token[i])
-                    old_pairs[old_pair] = old_pairs.get(old_pair, 0) + freq
-                    new_pair = (pre_token[i-1], new_token)
-                    tmp_new_pairs[new_pair] = tmp_new_pairs.get(new_pair, 0) + freq
-                if i+2 < len(pre_token):
-                    old_pair = (pre_token[i+1], pre_token[i+2])
-                    old_pairs[old_pair] = old_pairs.get(old_pair, 0) + freq
-                    new_pair = (new_token, pre_token[i+2])
-                    tmp_new_pairs[new_pair] = tmp_new_pairs.get(new_pair, 0) + freq
                 i += 2
             else:
                 new_pre_token.append(pre_token[i])
                 i += 1
-        # delete old pre_token, add new_pre_token
+        for i in range(len(pre_token)-1):
+            old_pair = (pre_token[i], pre_token[i+1])
+            old_pair_freq[old_pair] = old_pair_freq.get(old_pair, 0) + freq
+        for i in range(len(new_pre_token)-1):
+            new_pair = (new_pre_token[i], new_pre_token[i+1])
+            new_pair_freq[new_pair] = new_pair_freq.get(new_pair, 0) + freq
+            pair_pretoken_buffer.setdefault(new_pair, set()).add(tuple(new_pre_token))
+
         del pre_tokens[pre_token]
         pre_tokens[tuple(new_pre_token)] = freq
-        for new_pair, freq in tmp_new_pairs.items():
-            new_pairs[new_pair] = new_pairs.get(new_pair, 0) + freq
-            new_pairs_index.setdefault(new_pair, []).append(tuple(new_pre_token))
+    
+    for pair, freq in old_pair_freq.items():
+        pair_freq_buffer[pair] -= freq
+        if pair_freq_buffer[pair] == 0:
+            del pair_freq_buffer[pair]
+    for pair, freq in new_pair_freq.items():
+        pair_freq_buffer[pair] = pair_freq_buffer.get(pair, 0) + freq
 
-        # update buffers
-    for old_pair, freq in old_pairs.items():
-        pair_freq_buffer[old_pair] -= freq
-    for new_pair, freq in new_pairs.items():
-        pair_freq_buffer[new_pair] = pair_freq_buffer.get(new_pair, 0) + freq
-    # delete best_pair
-    del pair_freq_buffer[best_pair]
-    del pair_freq_index_buffer[best_pair]
     return best_pair
 
 def merge_with_buffer(
@@ -115,15 +110,15 @@ def merge_with_buffer(
     vocab_size: int,
 ) -> list[tuple[bytes, bytes]]:
     merges: list[tuple[bytes, bytes]] = []
-    # we buffer (pair, freq)
+    # we buffer (pair, freq), quickly find best_pair 
     pair_freq_buffer: dict[tuple[bytes, bytes], int] = {}
-    # we buffer (pair, pre_tokens)
-    pair_freq_index_buffer: dict[tuple[bytes, bytes], list[tuple[bytes, ...]]] = {}
-    _init_buffer(pre_tokens, pair_freq_buffer, pair_freq_index_buffer)
+    # we buffer (pair, list[pre_token]), quickly update pre_tokens
+    pair_pretoken_buffer: dict[tuple[bytes, bytes], set[tuple[bytes, ...]]] = {}
+    _init_buffer(pre_tokens, pair_freq_buffer, pair_pretoken_buffer)
     # merge util we reach vocab_size
     while len(vocab) < vocab_size:
         # get best pair from buffer and do updates
-        best_pair = _update_pre_tokens_with_buffer(pre_tokens, pair_freq_buffer, pair_freq_index_buffer)
+        best_pair = _update_with_buffer(pre_tokens, pair_freq_buffer, pair_pretoken_buffer)
         # update merges and vocab
         merges.append(best_pair)
         vocab[len(vocab)] = best_pair[0] + best_pair[1]
