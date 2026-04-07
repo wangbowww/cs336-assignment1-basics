5c75f3f0ec3a1b37c9e2e740a8b189b0050bc69f
wangbowww <weirdowww@qq.com>
2026-04-07 15:21:39 +0000

    modify __init__ import

--- cs336_basics/tokenizer/__init__.py
+++ cs336_basics/tokenizer/__init__.py
@@ -1,6 +1,2 @@
 from .train_bpe import train_bpe
-from .train_bpe import save_bpe_json
-from .train_bpe import load_bpe_vocab_json
-from .train_bpe import load_bpe_merges_json
-from .pretokenization import get_pre_tokens_from_sequence
 from .bpe_tokenizer import BPETokenizer
