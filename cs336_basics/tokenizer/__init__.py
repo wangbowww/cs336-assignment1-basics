0183adaa9ad04bab3f6160bf82770edc7aa88d02
wangbowww <weirdowww@qq.com>
2026-04-07 06:05:37 +0000

    adapt tokenizer to tests

--- cs336_basics/tokenizer/__init__.py
+++ cs336_basics/tokenizer/__init__.py
@@ -3,3 +3,4 @@ from .train_bpe import save_bpe_json
 from .train_bpe import load_bpe_vocab_json
 from .train_bpe import load_bpe_merges_json
 from .pretokenization import get_pre_tokens_from_sequence
+from .bpe_tokenizer import BPETokenizer
